e2d37cf3bbd484b7b0a97f5890448f73d77cf2cd
leejunghyeok <wndgur76@naver.com>
2024-01-16 22:28:58 +0900

    에러 수정

--- public/javascripts/modules/ThreeManager.js
+++ public/javascripts/modules/ThreeManager.js
@@ -2,35 +2,30 @@ import * as THREE from "three";
 
 export default class ThreeManager {
     static initScene() {
-        this.scene = new THREE.Scene({
-            antialias: true,
-        });
+        this.scene = new THREE.Scene();
 
         this.camera = new THREE.PerspectiveCamera(
-            100,
+            70,
             window.innerWidth / window.innerHeight,
             0.1,
             1000
         );
 
-        this.camera.position.set(0, -52, 96);
-        this.camera.lookAt(0, -28, 0);
+        this.camera.position.set(0, -64, 192);
+        this.camera.lookAt(0, -24, 0);
         this.scene.add(this.camera);
-
-        this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.domElement.id = "scene";
-        document.getElementById("game").appendChild(this.renderer.domElement);
-
-        window.addEventListener("resize", () => {
-            this.camera.aspect = window.innerHeight / window.innerWidth;
-            this.camera.updateProjectionMatrix();
-            this.renderer.setSize(window.innerWidth, window.innerHeight);
-        });
 
         let light = new THREE.HemisphereLight(0xffffff, 0x000000, 2);
         light.position.set(0, 0, 100);
         this.scene.add(light);
+
+        // const dirLight = new THREE.DirectionalLight(0xffffff, 5);
+        // dirLight.position.set(0, 0, 700);
+        // this.scene.add(dirLight);
+
+        const pointLight = new THREE.PointLight(0xffffff, 100);
+        pointLight.position.set(7, -5, -5);
+        this.scene.add(pointLight);
 
         const boxWidth = 30;
         const boxHeight = 24;
@@ -38,6 +33,7 @@ export default class ThreeManager {
 
         let cubeGeometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
         let cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x912431 });
+
         // 5 x 6 board
         for (let i = 0; i < 30; ++i) {
             const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
@@ -62,6 +58,7 @@ export default class ThreeManager {
         // 1 x 7 queue
         cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x283482 });
         let y = -((boxHeight + 1) * 3.5) + (boxHeight - smallBoxHeight) / 2;
+
         for (let i = 0; i < 7; i++) {
             const cube = new THREE.Mesh(smallCubeGeometry, cubeMaterial);
             cube.translateY(y);
@@ -73,6 +70,7 @@ export default class ThreeManager {
 
         cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x238742 });
         y = (boxHeight + 1) * 3.5 - (boxHeight - smallBoxHeight) / 2;
+
         for (let i = 0; i < 7; i++) {
             const cube = new THREE.Mesh(smallCubeGeometry, cubeMaterial);
             cube.translateY(y);
@@ -81,10 +79,37 @@ export default class ThreeManager {
             );
             this.scene.add(cube);
         }
+
+        const sphere = new THREE.SphereGeometry(20, 32, 32);
+        const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+        this.sphereMesh = new THREE.Mesh(sphere, sphereMaterial);
+        this.scene.add(this.sphereMesh);
+
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true,
+        });
+
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.domElement.id = "scene";
+        this.renderer.setClearColor(0x81ecec, 0.5);
+        document.body.appendChild(this.renderer.domElement);
+
+        window.onresize = () => {
+            ThreeManager.camera.aspect = window.innerWidth / window.innerHeight;
+            ThreeManager.camera.updateProjectionMatrix();
+            ThreeManager.renderer.setSize(
+                window.innerWidth,
+                window.innerHeight
+            );
+        };
+
+        this.renderer.render(this.scene, this.camera);
+        this.animate();
     }
 
     static animate() {
-        requestAnimationFrame(this.animate);
-        this.renderer.render(this.scene, this.camera);
+        requestAnimationFrame(ThreeManager.animate);
+        ThreeManager.renderer.render(ThreeManager.scene, ThreeManager.camera);
     }
 }
