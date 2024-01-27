c136f2fc888ac22b31eb9d78323e4a6ef3031a46
leejunghyeok <wndgur76@naver.com>
2024-01-27 21:14:54 +0900

    스킬 구현중

--- modules/constants/SKILLS.js
+++ modules/constants/SKILLS.js
@@ -1,9 +1,21 @@
+const INFINITY = -1;
+const TYPES = {
+    cc: "cc",
+    damage: "damage",
+    heal: "heal",
+};
 const SKILLS = {
     roar: {
         id: "roar",
         name: "포효",
         desc: "주변의 적들을 1초간 기절시킵니다.",
-        mp: 20,
+        mp: 80,
+        range: 1,
+        active: true,
+        targetAmount: INFINITY,
+        type: TYPES.cc,
+        subType: "stun",
+        value: 1,
         execute: function () {
             console.log(this.name, "가 포효합니다.");
         },
