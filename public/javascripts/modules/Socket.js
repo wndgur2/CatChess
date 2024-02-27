0cf6d1f8d99ea093cea6353c5c4caf2f948880dc
leejunghyeok <wndgur76@naver.com>
2024-02-27 13:28:27 +0900

    공통 코드 함수화, 테스트동안 로컬 서버 이용하도록

--- public/javascripts/modules/Socket.js
+++ public/javascripts/modules/Socket.js
@@ -11,19 +11,16 @@ export default class Socket {
     static id = localStorage.getItem(CATCHESS_ID);
 
     static init() {
-        // Socket.socket = new WebSocket("ws://localhost:8080");
-        Socket.socket = new WebSocket(
-            "ws://catchess.ap-northeast-2.elasticbeanstalk.com:8080"
-        );
+        Socket.socket = new WebSocket("ws://localhost:8080");
+        // Socket.socket = new WebSocket(
+        //     "ws://catchess.ap-northeast-2.elasticbeanstalk.com:8080"
+        // );
 
         Socket.socket.onopen = function (event) {
             console.log("web socket connected.");
             if (!Socket.id) Socket.sendMsg("reqNewId", null);
             else {
-                document.getElementById("id").innerHTML = Socket.id;
-                const playBtn = document.getElementById("playBtn");
-                playBtn.className = "btnActive";
-                playBtn.innerHTML = "<span>Match</span>";
+                readyToPlay();
             }
         };
 
@@ -36,8 +33,8 @@ export default class Socket {
             switch (type) {
                 case "resNewId": {
                     Socket.id = data;
-                    document.getElementById("id").innerHTML = data;
                     localStorage.setItem(CATCHESS_ID, data);
+                    readyToPlay();
                     break;
                 }
                 case "gameMatched": {
@@ -183,4 +180,11 @@ export default class Socket {
             })
         );
     }
+}
+
+function readyToPlay() {
+    document.getElementById("id").innerHTML = Socket.id;
+    const playBtn = document.getElementById("playBtn");
+    playBtn.className = "btnActive";
+    playBtn.innerHTML = "<span>Match</span>";
 }
