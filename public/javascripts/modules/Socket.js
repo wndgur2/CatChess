cc2674f558a0534ab1ef275dc760c81fad80e836
leejunghyeok <wndgur76@naver.com>
2024-03-22 19:06:29 +0900

    3.22

--- public/javascripts/modules/Socket.js
+++ public/javascripts/modules/Socket.js
@@ -11,8 +11,8 @@ export default class Socket {
     static id = localStorage.getItem(CATCHESS_ID);
 
     static init() {
-        const url = "ws://localhost:8080";
-        // const url = "ws://catchess.ap-northeast-2.elasticbeanstalk.com:8080";
+        // const url = "ws://localhost:8080";
+        const url = "ws://catchess.ap-northeast-2.elasticbeanstalk.com:8080";
         Socket.socket = new WebSocket(url);
 
         Socket.socket.onopen = function (event) {
