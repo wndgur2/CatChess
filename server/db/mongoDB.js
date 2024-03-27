5c1e3071f33b0b9a283116917e3bf570f18863e2
leejunghyeok <wndgur76@naver.com>
2024-03-27 15:14:05 +0900

    deprecated functions removed

--- server/db/mongoDB.js
+++ server/db/mongoDB.js
@@ -2,10 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 require("./schema/User");
 
-mongoose.connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(process.env.DB_URI);
 
 const db = mongoose.connection;
 db.on("error", (error) => {
