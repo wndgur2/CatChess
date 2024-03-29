61acbd559219dd61b31acbec3b19e5a077bb44aa
leejunghyeok <wndgur76@naver.com>
2024-03-29 15:18:53 +0900

    win, lose log api 추가

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const mongoDB = require("../db/mongoDB");
+const jwt = require("jsonwebtoken");
 
 router.get("/cats", (req, res, next) => {
     res.json(require("../modules/constants/cats.json"));
@@ -12,13 +13,17 @@ router.get("/init", (req, res, next) => {
     res.send(req.ip);
 });
 router.post("/log/browser", (req, res, next) => {
-    console.log("Browser info:", req.body);
+    // console.log("Browser info:", req.body);
     res.send("success");
 });
 
 // seperate db apis
 router.get("/user/init", async (req, res, next) => {
-    const email = req.query.email;
+    const id_token = req.headers.authorization
+        .split('"id_token":"')[1]
+        .split('"')[0];
+    const email = jwt.decode(id_token).email;
+    console.log(email);
     const user = await initUser(email);
     res.json(user);
 });
@@ -37,4 +42,37 @@ async function initUser(email) {
     }
     return user;
 }
+
+router.put("/user/win", async (req, res, next) => {
+    const id_token = req.headers.authorization
+        .split('"id_token":"')[1]
+        .split('"')[0];
+    const email = jwt.decode(id_token).email;
+    const user = await updateUser(email, "win");
+    res.json(user);
+});
+
+router.put("/user/lose", async (req, res, next) => {
+    const id_token = req.headers.authorization
+        .split('"id_token":"')[1]
+        .split('"')[0];
+    const email = jwt.decode(id_token).email;
+    const user = await updateUser(email, "lose");
+    res.json(user);
+});
+
+async function updateUser(email, result) {
+    const User = mongoDB.model("User");
+    let user = await User.findOne({
+        email: email,
+    });
+    if (result === "win") {
+        user.win++;
+    } else {
+        user.loss++;
+    }
+    await user.save();
+    return user;
+}
+
 module.exports = router;
